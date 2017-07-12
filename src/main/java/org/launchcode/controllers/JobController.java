757fe6c7e59ab824974aa0333b846dfb8a7dc506
Constance <catubbs88@yahoo.com>
2017-07-12 11:44:39 -0500

    Completed first two TODOs: /job?id=[id] functionality

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -22,9 +23,9 @@ public class JobController {
     // The detail display for a given Job at URLs like /job?id=17
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model, int id) {
-
-        // TODO #1 - get the Job with the given ID and pass it into the view
-
+        Job job = jobData.findById(id);
+        model.addAttribute("job", job);
+        model.addAttribute("title", job.getName());
         return "job-detail";
     }
 
